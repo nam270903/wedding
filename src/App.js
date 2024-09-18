ea7935422fd46011882666de5181991a29404241
nam270903 <nam270903@gmail.com>
2024-09-19 01:36:49 +0700

    ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import React from 'react';
+import './styles.css';
+import image2 from './assets/image2.jpg'
 
 function App() {
   return (
-  <p>Hello world</p>
+    <div className="App">
+      <div className="content">
+        <h1>Wedding Invitation</h1>
+
+        <div>
+          <img src={image2} alt="Wedding Invitation" />
+        </div>
+
+        <div className="Invitation">
+          <p>WE WOULD LIKE TO REQUEST </p>
+          <p>THE PLEASURE OF YOUR COMPANY</p>
+          <p> AT THE CELEBRATION OF OUR MARRIAGE</p>
+        </div>
+
+        <div className="Name"> 
+          <p>Vũ Quang Minh</p>
+          <p className="and"> and </p>
+          <p> Đặng Vân Anh </p>
+        </div>
+
+        <div className='LocaBold'>
+          <p>SEN EVENT & WEDDING</p>
+        </div>
+
+        <div className="Location">
+          <p>614 LAC LONG QUAN, TAY HO, HA NOI</p>
+        </div>
+
+        <div className="Register">
+          <p>Register</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
